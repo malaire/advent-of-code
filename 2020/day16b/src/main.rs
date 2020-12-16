fa35f960e6c66c78ee91bea3e2e189a334fa9baa
Markus Laire <malaire@gmail.com>
2020-12-16 17:46:31 +0200

    day16b: Rewrite

--- 2020/day16b/src/main.rs
+++ 2020/day16b/src/main.rs
@@ -1,5 +1,4 @@
 use regex::Regex;
-use std::collections::{HashMap, HashSet};
 
 static INPUT_A: &str = "departure class: 0-1 or 4-19
 departure row: 0-5 or 8-19
@@ -29,7 +28,13 @@ struct Field {
     max_a: usize,
     min_b: usize,
     max_b: usize,
-    potential_positions: HashSet<usize>,
+    potential_positions_bitmap: u64,
+}
+
+impl Field {
+    fn is_valid(&self, n: usize) -> bool {
+        (n >= self.min_a && n <= self.max_a) || (n >= self.min_b && n <= self.max_b)
+    }
 }
 
 fn solve(input: &str) -> usize {
@@ -38,71 +43,33 @@ fn solve(input: &str) -> usize {
     let mut fields: Vec<Field> = Vec::new();
     let mut my_ticket: Vec<usize> = Vec::new();
 
-    let mut next_is_my_ticket = false;
+    let mut next_is_my_ticket = true;
     for line in input.lines() {
-        if re_rule.is_match(line) {
-            let cap = re_rule.captures(line).unwrap();
-            let name = cap[1].to_owned();
-            let min_a: usize = cap[2].parse().unwrap();
-            let max_a: usize = cap[3].parse().unwrap();
-            let min_b: usize = cap[4].parse().unwrap();
-            let max_b: usize = cap[5].parse().unwrap();
+        if let Some(cap) = re_rule.captures(line) {
             fields.push(Field {
-                name,
-                min_a,
-                max_a,
-                min_b,
-                max_b,
-                potential_positions: HashSet::new(),
+                name: cap[1].to_owned(),
+                min_a: cap[2].parse().unwrap(),
+                max_a: cap[3].parse().unwrap(),
+                min_b: cap[4].parse().unwrap(),
+                max_b: cap[5].parse().unwrap(),
+                potential_positions_bitmap: 0,
             });
-        } else if line == "your ticket:" {
-            next_is_my_ticket = true;
-        } else if line == "nearby tickets:" || line == "" {
-            // IGNORE
-        } else {
+        } else if line.contains(',') {
+            let ticket: Vec<usize> = line.split(",").map(|n| n.parse().unwrap()).collect();
             if next_is_my_ticket {
-                my_ticket = line
-                    .split(",")
-                    .map(|n| n.parse::<usize>().unwrap())
-                    .collect();
-
-                let pos_count = my_ticket.len();
+                next_is_my_ticket = false;
+                my_ticket = ticket;
 
                 for field in &mut fields {
-                    for pos in 0..pos_count {
-                        field.potential_positions.insert(pos);
-                    }
+                    field.potential_positions_bitmap = (1 << my_ticket.len()) - 1;
                 }
-
-                next_is_my_ticket = false;
             } else {
-                let mut discard = false;
-                for n in line.split(",").map(|n| n.parse::<usize>().unwrap()) {
-                    let mut can_be_valid = false;
-                    for field in &fields {
-                        if (n >= field.min_a && n <= field.max_a)
-                            || (n >= field.min_b && n <= field.max_b)
-                        {
-                            can_be_valid = true;
-                        }
-                    }
-                    if !can_be_valid {
-                        discard = true;
-                    }
-                }
-
-                if !discard {
-                    for (pos, n) in line
-                        .split(",")
-                        .map(|n| n.parse::<usize>().unwrap())
-                        .enumerate()
-                    {
+                // ticket is valid if ALL numbers match ANY field
+                if ticket.iter().all(|n| fields.iter().any(|f| f.is_valid(*n))) {
+                    for (pos, n) in ticket.iter().enumerate() {
                         for field in fields.iter_mut() {
-                            if n < field.min_a
-                                || (n > field.max_a && n < field.min_b)
-                                || (n > field.max_b)
-                            {
-                                field.potential_positions.remove(&pos);
+                            if !field.is_valid(*n) {
+                                field.potential_positions_bitmap &= !(1 << pos);
                             }
                         }
                     }
@@ -111,41 +78,28 @@ fn solve(input: &str) -> usize {
         }
     }
 
-    // println!("{:#?}", fields);
-
     let mut unsolved_fields = fields;
-    let mut solved: HashMap<String, usize> = HashMap::new();
+    let mut unsolved_positions_bitmap: u64 = (1 << my_ticket.len()) - 1;
+    let mut solved_product = 1;
 
-    while unsolved_fields.len() > 0 {
-        let mut new_solved_pos: Vec<usize> = Vec::new();
-        for field in &unsolved_fields {
-            if field.potential_positions.len() == 1 {
-                let pos = *field.potential_positions.iter().next().unwrap();
-                solved.insert(field.name.clone(), pos);
-                new_solved_pos.push(pos);
+    while unsolved_positions_bitmap != 0 {
+        unsolved_fields.retain(|field| {
+            if field.potential_positions_bitmap.count_ones() == 1 {
+                let pos = field.potential_positions_bitmap.trailing_zeros() as usize;
+                unsolved_positions_bitmap ^= 1 << pos;
+                if field.name.starts_with("departure ") {
+                    solved_product *= my_ticket[pos];
+                }
+                false
+            } else {
+                true
             }
-        }
-
-        unsolved_fields = unsolved_fields
-            .into_iter()
-            .filter(|f| f.potential_positions.len() != 1)
-            .collect();
+        });
 
         for field in &mut unsolved_fields {
-            for pos in &new_solved_pos {
-                field.potential_positions.remove(&pos);
-            }
+            field.potential_positions_bitmap &= unsolved_positions_bitmap;
         }
     }
 
-    // println!("SOLVED\n{:#?}", solved);
-
-    let mut mul = 1;
-    for (name, pos) in &solved {
-        if name.starts_with("departure ") {
-            mul *= my_ticket[*pos];
-        }
-    }
-
-    mul
+    solved_product
 }
