b9cffcbb00a476b19a86dbe9ea79c677d7a6827b
Markus Laire <malaire@gmail.com>
2020-12-18 12:59:37 +0200

    day18: Rewrite to use custom Replacer

--- 2020/day18a/src/main.rs
+++ 2020/day18a/src/main.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use regex::{Captures, Regex};
 
 static INPUT_A: &str = "1 + 2 * 3 + 4 * 5 + 6
 1 + (2 * 3) + (4 * (5 + 6))";
@@ -12,35 +12,78 @@ fn main() {
     println!("{:?}", solve(INPUT_X));
 }
 
-fn solve(input: &str) -> usize {
-    let re_one_op = Regex::new(r"^(.*\()(\d+) ([+*]) (\d+)(.+)$").unwrap();
-    let re_number_in_parentheses = Regex::new(r"^(.*)\((\d+)\)(.+)$").unwrap();
-    let re_done = Regex::new(r"^\((\d+)\)$").unwrap();
+fn calculate(expr: &str) -> usize {
+    let mut re_parentheses = Replacer::new(r"\(([^()]+)\)", |c| calculate(&c[1]));
+    let mut re_add_or_mul = Replacer::new(r"(\d+) ([+*]) (\d+)", |c| {
+        if &c[2] == "+" {
+            c[1].to_usize() + c[3].to_usize()
+        } else {
+            c[1].to_usize() * c[3].to_usize()
+        }
+    });
 
+    let mut expr = expr.to_string();
+    while re_parentheses.replace(&mut expr) || re_add_or_mul.replace(&mut expr) {}
+    expr.to_usize()
+}
+
+fn solve(input: &str) -> usize {
     let mut sum = 0;
     for line in input.lines() {
-        let mut line = format!("({})", line);
-        sum += loop {
-            if let Some(cap) = re_one_op.captures(&line) {
-                let pre = &cap[1];
-                let a = cap[2].parse::<usize>().unwrap();
-                let op = &cap[3];
-                let b = cap[4].parse::<usize>().unwrap();
-                let post = &cap[5];
-                if op == "+" {
-                    line = format!("{}{}{}", pre, a + b, post);
-                } else {
-                    line = format!("{}{}{}", pre, a * b, post);
-                }
-            } else if let Some(cap) = re_number_in_parentheses.captures(&line) {
-                let pre = &cap[1];
-                let n = cap[2].parse::<usize>().unwrap();
-                let post = &cap[3];
-                line = format!("{}{}{}", pre, n, post);
-            } else if let Some(cap) = re_done.captures(&line) {
-                break cap[1].parse::<usize>().unwrap();
+        sum += calculate(line);
+    }
+    sum
+}
+
+// ================================================================================
+// StrExt
+
+trait StrExt {
+    fn to_usize(&self) -> usize;
+}
+
+impl StrExt for str {
+    fn to_usize(&self) -> usize {
+        self.parse::<usize>().unwrap()
+    }
+}
+
+// ================================================================================
+// Replacer
+
+struct Replacer<R, S>
+where
+    R: FnMut(&Captures) -> S,
+    S: ToString,
+{
+    regex: Regex,
+    replacer: R,
+}
+
+impl<R, S> Replacer<R, S>
+where
+    R: FnMut(&Captures) -> S,
+    S: ToString,
+{
+    fn new(regex: &str, replacer: R) -> Self {
+        Replacer {
+            regex: Regex::new(regex).unwrap(),
+            replacer,
+        }
+    }
+
+    /// Replace first match. Return `true` if match was found.
+    fn replace(&mut self, text: &mut String) -> bool {
+        let replacer = &mut self.replacer;
+        match self
+            .regex
+            .replace(&text, |c: &Captures| replacer(c).to_string())
+        {
+            std::borrow::Cow::Borrowed(_) => false,
+            std::borrow::Cow::Owned(new) => {
+                *text = new;
+                true
             }
         }
     }
-    sum
 }
--- 2020/day18b/src/main.rs
+++ 2020/day18b/src/main.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use regex::{Captures, Regex};
 
 static INPUT_A: &str = "1 + 2 * 3 + 4 * 5 + 6
 1 + (2 * 3) + (4 * (5 + 6))
@@ -20,34 +20,16 @@ fn main() {
 }
 
 fn calculate(expr: &str) -> usize {
-    let re_parentheses = Regex::new(r"^(.+)\(([^()]+)\)(.+)$").unwrap();
-    let re_add = Regex::new(r"^(.*?)(\d+) \+ (\d+)(.+)$").unwrap();
-    let re_mul = Regex::new(r"^(.*?)(\d+) \* (\d+)(.+)$").unwrap();
-    let re_done = Regex::new(r"^\((\d+)\)$").unwrap();
+    let mut re_parentheses = Replacer::new(r"\(([^()]+)\)", |c| calculate(&c[1]));
+    let mut re_add = Replacer::new(r"(\d+) \+ (\d+)", |c| c[1].to_usize() + c[2].to_usize());
+    let mut re_mul = Replacer::new(r"(\d+) \* (\d+)", |c| c[1].to_usize() * c[2].to_usize());
 
-    let mut expr = format!("({})", expr);
-    loop {
-        if let Some(cap) = re_parentheses.captures(&expr) {
-            let pre = &cap[1];
-            let sub_expr = &cap[2];
-            let post = &cap[3];
-            expr = format!("{}{}{}", pre, calculate(sub_expr), post);
-        } else if let Some(cap) = re_add.captures(&expr) {
-            let pre = &cap[1];
-            let a = cap[2].parse::<usize>().unwrap();
-            let b = cap[3].parse::<usize>().unwrap();
-            let post = &cap[4];
-            expr = format!("{}{}{}", pre, a + b, post);
-        } else if let Some(cap) = re_mul.captures(&expr) {
-            let pre = &cap[1];
-            let a = cap[2].parse::<usize>().unwrap();
-            let b = cap[3].parse::<usize>().unwrap();
-            let post = &cap[4];
-            expr = format!("{}{}{}", pre, a * b, post);
-        } else if let Some(cap) = re_done.captures(&expr) {
-            return cap[1].parse::<usize>().unwrap();
-        }
-    }
+    let mut expr = expr.to_string();
+    while re_parentheses.replace(&mut expr)
+        || re_add.replace(&mut expr)
+        || re_mul.replace(&mut expr)
+    {}
+    expr.to_usize()
 }
 
 fn solve(input: &str) -> usize {
@@ -56,4 +38,57 @@ fn solve(input: &str) -> usize {
         sum += calculate(line);
     }
     sum
+}
+
+// ================================================================================
+// StrExt
+
+trait StrExt {
+    fn to_usize(&self) -> usize;
+}
+
+impl StrExt for str {
+    fn to_usize(&self) -> usize {
+        self.parse::<usize>().unwrap()
+    }
+}
+
+// ================================================================================
+// Replacer
+
+struct Replacer<R, S>
+where
+    R: FnMut(&Captures) -> S,
+    S: ToString,
+{
+    regex: Regex,
+    replacer: R,
+}
+
+impl<R, S> Replacer<R, S>
+where
+    R: FnMut(&Captures) -> S,
+    S: ToString,
+{
+    fn new(regex: &str, replacer: R) -> Self {
+        Replacer {
+            regex: Regex::new(regex).unwrap(),
+            replacer,
+        }
+    }
+
+    /// Replace first match. Return `true` if match was found.
+    fn replace(&mut self, text: &mut String) -> bool {
+        let replacer = &mut self.replacer;
+        match self
+            .regex
+            .replace(&text, |c: &Captures| replacer(c).to_string())
+        {
+            std::borrow::Cow::Borrowed(_) => false,
+            std::borrow::Cow::Owned(new) => {
+                *text = new;
+                true
+            }
+        }
+    }
 }
