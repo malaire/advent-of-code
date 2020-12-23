e36e9c8aa43a837a0d2b29f96bbc293944071c90
Markus Laire <malaire@gmail.com>
2020-12-23 09:42:56 +0200

    day23: Combine parts

--- 2020/day23ab/src/main.rs
+++ 2020/day23ab/src/main.rs
@@ -1,16 +1,67 @@
 static INPUT_A: &str = "389125467";
 static INPUT_X: &str = "157623984";
 
-const MAX_CUP: usize = 1_000_000;
-const MOVES: usize = 10_000_000;
-
 fn main() {
-    assert_eq!(solve(INPUT_A, 10, false), 9 * 2);
-    assert_eq!(solve(INPUT_A, MOVES, true), 149245887792);
-    assert_eq!(solve(INPUT_X, MOVES, true), 111057672960);
+    // PART 1
 
-    println!("{:?}", solve(INPUT_X, MOVES, true));
+    assert_eq!(solve_1(INPUT_A, 10), 92658374);
+    assert_eq!(solve_1(INPUT_A, 100), 67384529);
+    assert_eq!(solve_1(INPUT_X, 100), 58427369);
+
+    println!("{:?}", solve_1(INPUT_X, 100));
+
+    // PART 2
+
+    assert_eq!(solve_2(INPUT_A, 10, false), 9 * 2);
+    assert_eq!(solve_2(INPUT_A, 10_000_000, true), 149245887792);
+    assert_eq!(solve_2(INPUT_X, 10_000_000, true), 111057672960);
+
+    println!("{:?}", solve_2(INPUT_X, 10_000_000, true));
 }
+
+fn solve_1(input: &str, moves: usize) -> usize {
+    let initial_cups = parse_input(input);
+
+    let mut current_cup = initial_cups[0];
+    let mut cups = Ring::new(initial_cups);
+
+    for _ in 0..moves {
+        current_cup = cups.move_three(current_cup);
+    }
+
+    cups.solution_1()
+}
+
+fn solve_2(input: &str, moves: usize, fill_to_million: bool) -> usize {
+    let mut initial_cups = parse_input(input);
+
+    if fill_to_million {
+        for cup in (initial_cups.len() + 1)..=1_000_000 {
+            initial_cups.push(cup);
+        }
+    }
+
+    let mut current_cup = initial_cups[0];
+    let mut cups = Ring::new(initial_cups);
+
+    for _ in 0..moves {
+        current_cup = cups.move_three(current_cup);
+    }
+
+    cups.solution_2()
+}
+
+// ================================================================================
+// HELPERS
+
+fn parse_input(input: &str) -> Vec<usize> {
+    input
+        .chars()
+        .map(|ch| (ch as usize) - ('0' as usize))
+        .collect()
+}
+// ================================================================================
+// Ring
 
 struct Node {
     cup: usize,
@@ -79,33 +130,21 @@ impl Ring {
         next_current_cup
     }
 
-    fn solution(&self) -> usize {
+    fn solution_1(&self) -> usize {
+        let mut solution = 0;
+        let mut pos = self.positions[1];
+        for _ in 0..(self.len - 1) {
+            pos = self.nodes[pos].next_pos;
+            solution *= 10;
+            solution += self.nodes[pos].cup;
+        }
+        solution
+    }
+
+    fn solution_2(&self) -> usize {
         let pos_1 = &self.nodes[self.positions[1]];
         let a = &self.nodes[pos_1.next_pos];
         let b = &self.nodes[a.next_pos];
         a.cup * b.cup
     }
-}
-
-fn solve(input: &str, moves: usize, fill: bool) -> usize {
-    let mut initial_cups: Vec<usize> = input
-        .chars()
-        .map(|ch| (ch as usize) - ('0' as usize))
-        .collect();
-
-    if fill {
-        for cup in (initial_cups.len() + 1)..=MAX_CUP {
-            initial_cups.push(cup);
-        }
-        assert!(initial_cups.len() == MAX_CUP);
-    }
-
-    let mut current_cup = initial_cups[0];
-    let mut cups = Ring::new(initial_cups);
-
-    for _ in 0..moves {
-        current_cup = cups.move_three(current_cup);
-    }
-
-    cups.solution()
 }
