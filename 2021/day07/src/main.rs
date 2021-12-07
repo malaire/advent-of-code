f581c2a107dda21fa4d75ddfb609be1c18769815
Markus Laire <malaire@gmail.com>
2021-12-07 16:03:43 +0200

    day07: unify parts and use malaire-aoc

--- 2021/day07/src/main.rs
+++ 2021/day07/src/main.rs
@@ -1,57 +1,45 @@
+use malaire_aoc::prelude::*;
+
 static INPUT_A: &str = "16,1,2,0,4,2,7,1,2,14";
 static INPUT_X: &str = include_str!("input");
 
 fn main() {
-    assert_eq!(solve_1(INPUT_A), 37);
-    let x = solve_1(INPUT_X);
-    assert_eq!(x, 340056);
-    println!("{:?}", x);
+    run(0, solve_1, INPUT_A, 37);
+    run(1, solve_1, INPUT_X, 340056);
 
-    assert_eq!(solve_2(INPUT_A), 168);
-    let x = solve_2(INPUT_X);
-    assert_eq!(x, 96592275);
-    println!("{:?}", x);
+    run(0, solve_2, INPUT_A, 168);
+    run(2, solve_2, INPUT_X, 96592275);
 }
 
 fn solve_1(input: &str) -> isize {
-    let input: Vec<isize> = input
-        .trim()
-        .split(',')
-        .map(|x| x.parse().unwrap())
-        .collect();
-
-    let min = *input.iter().min().unwrap();
-    let max = *input.iter().max().unwrap();
-
-    let mut min_fuel = isize::MAX;
-    for target in min..=max {
-        let mut fuel = 0;
-        for pos in &input {
-            fuel += (pos - target).abs();
-        }
-        min_fuel = min_fuel.min(fuel);
-    }
-
-    min_fuel
+    solve_generic(input, true)
 }
 
 fn solve_2(input: &str) -> isize {
-    let input: Vec<isize> = input
-        .trim()
-        .split(',')
-        .map(|x| x.parse().unwrap())
-        .collect();
+    solve_generic(input, false)
+}
+
+fn solve_generic(input: &str, constant_rate: bool) -> isize {
+    let input = parse_numbers::<isize>(input);
 
     let min = *input.iter().min().unwrap();
     let max = *input.iter().max().unwrap();
 
     let mut min_fuel = isize::MAX;
+
     for target in min..=max {
         let mut fuel = 0;
+
         for pos in &input {
             let delta = (pos - target).abs();
-            fuel += (1 + delta) * delta / 2;
+
+            if constant_rate {
+                fuel += delta;
+            } else {
+                fuel += delta * (1 + delta) / 2;
+            }
         }
+
         min_fuel = min_fuel.min(fuel);
     }
 
