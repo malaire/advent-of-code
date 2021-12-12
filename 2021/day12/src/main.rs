a798057cc42f9408d074dc3d539b9268557e028b
Markus Laire <malaire@gmail.com>
2021-12-12 08:12:06 +0200

    day12: improve code, unify parts

--- 2021/day12/src/main.rs
+++ 2021/day12/src/main.rs
@@ -21,42 +21,18 @@ fn main() {
 }
 
 fn solve_1(input: &str) -> usize {
-    visit_all_1("start", &mut HashSet::new(), &read_input(input))
-}
-
-fn visit_all_1(
-    current: &str,
-    visited: &mut HashSet<String>,
-    connections: &HashMap<String, HashSet<String>>,
-) -> usize {
-    if current == "end" {
-        return 1;
-    }
-
-    let mut path_count = 0;
-    for next in connections.get(current).unwrap() {
-        let next_is_big = next.as_bytes()[0].is_ascii_uppercase();
-
-        if next != "start" && (next_is_big || !visited.contains(next)) {
-            if !next_is_big {
-                visited.insert(next.to_string());
-            }
-            path_count += visit_all_1(next, visited, connections);
-            visited.remove(next);
-        }
-    }
-    path_count
+    visit_all("start", &mut HashSet::new(), &read_input(input), false)
 }
 
 fn solve_2(input: &str) -> usize {
-    visit_all_2("start", &mut HashSet::new(), &read_input(input), false)
+    visit_all("start", &mut HashSet::new(), &read_input(input), true)
 }
 
-fn visit_all_2(
+fn visit_all(
     current: &str,
     visited: &mut HashSet<String>,
     connections: &HashMap<String, HashSet<String>>,
-    visited_twice: bool,
+    can_visit_twice: bool,
 ) -> usize {
     if current == "end" {
         return 1;
@@ -66,16 +42,14 @@ fn visit_all_2(
     for next in connections.get(current).unwrap() {
         let next_is_big = next.as_bytes()[0].is_ascii_uppercase();
 
-        if next != "start" && (next_is_big || !visited.contains(next) || !visited_twice) {
-            if next_is_big {
-                path_count += visit_all_2(next, visited, connections, visited_twice);
-            } else if !visited.contains(next) {
-                visited.insert(next.to_string());
-                path_count += visit_all_2(next, visited, connections, visited_twice);
-                visited.remove(next);
-            } else {
-                path_count += visit_all_2(next, visited, connections, true);
-            }
+        if next_is_big {
+            path_count += visit_all(next, visited, connections, can_visit_twice);
+        } else if !visited.contains(next) {
+            visited.insert(next.to_string());
+            path_count += visit_all(next, visited, connections, can_visit_twice);
+            visited.remove(next);
+        } else if can_visit_twice {
+            path_count += visit_all(next, visited, connections, false);
         }
     }
     path_count
@@ -91,11 +65,16 @@ fn read_input(input: &str) -> HashMap<String, HashSet<String>> {
         let a = parts[0].to_owned();
         let b = parts[1].to_owned();
 
-        connections
-            .entry(a.clone())
-            .or_insert(HashSet::new())
-            .insert(b.clone());
-        connections.entry(b).or_insert(HashSet::new()).insert(a);
+        if b != "start" {
+            connections
+                .entry(a.clone())
+                .or_insert(HashSet::new())
+                .insert(b.clone());
+        }
+
+        if a != "start" {
+            connections.entry(b).or_insert(HashSet::new()).insert(a);
+        }
     }
     connections
 }
